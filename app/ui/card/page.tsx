d4fa2b8c58da1d4ef892a697b156dc71aedb9141
Raphaël Monteiro <monterap@students.zhaw.ch>
2024-07-21 13:36:50 +0200

    added cards to dashboard

---
+++ app/ui/card/page.tsx
@@ -0,0 +1,8 @@
+export default function Card() {
+    return (
+        <div className="max-w-full rounded overflow-hidden shadow m-2 p-2">
+            <h2>Title</h2>
+            <img src="" alt="Card Image"/>
+        </div>
+    );
+}
--- app/ui/dashboard/page.tsx
+++ app/ui/dashboard/page.tsx
@@ -1,6 +1,8 @@
+import Card from "@/app/ui/card/page"
+
 export default function Dashboard () {
     return (
-        <div className="flex-col flex-1">
+        <div className="flex flex-col flex-1 h-screen">
             <div className="flex items-center bg-gray-200 p-2">
                 <button className="hamburger p-2">
                     <span></span>
@@ -9,7 +11,24 @@ export default function Dashboard () {
                 </button>
                 <div>Dashboard</div>
             </div>
-            <h1>Hello!</h1>
+
+            <div className="flex flex-1">
+                <div className="flex flex-col flex-1">
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+
+                <div className="flex-1">
+                    <Card />
+                </div>
+
+                <div className="flex flex-col flex-1">
+                    <Card />
+                    <Card />
+                    <Card />
+                </div>
+            </div>
         </div>
     );
 }
